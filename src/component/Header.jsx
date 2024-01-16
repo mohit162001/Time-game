76ec4dbbf75457bde0c700a8e1eb65b2adcc4c3c
Mohit Devade <mohit.deavade123@gmail.com>
2024-01-16 17:08:18 +0530

    header component  added

---
+++ src/component/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div id="content">
+      <header>
+        <h1>
+          The <span>Time</span> Game
+        </h1>
+        <p>Stop the timer once you estimate the time</p>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
